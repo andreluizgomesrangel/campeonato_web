8527a86fc21d6027f63963f45daf46fef6185aba
Andre Luiz Gomes Rangel dos Santos <andreluiz.grs@gmail.com>
2017-03-23 14:48:17 -0300

    - Tela atualiza sozinha a cada 7 segundos. - Tem um bug - o ultimo botão pressionado eh pressionado sozinho a cada atualização da tela.

--- src/main/java/br/com/mobilesaude/cliente/CGolPost.java
+++ src/main/java/br/com/mobilesaude/cliente/CGolPost.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
 import java.util.List;
 
 import javax.xml.bind.JAXBContext;
@@ -55,22 +57,33 @@ public class CGolPost {
 		        .post(formBody)
 		        .build();
 	
+		
+		
 		try {
 		    Response response = client.newCall(request).execute();
-		    System.out.println(response.body().string());
+		    
+		    String xmlString = new String(response.body().string());
+		    //System.out.println(response.body().string());
+		    //System.out.println(response.body().string());
+		    
+		    ///response.body().t
 		    
 		    // convert String into InputStream
-		    InputStream is = new ByteArrayInputStream(response.body().toString().getBytes());
+		   // InputStream is = new ByteArrayInputStream(response.body().toString().getBytes());
 		    
 		    
 		    // read it with BufferedReader
-		    BufferedReader br = new BufferedReader(new InputStreamReader(is) );
+		   // BufferedReader br = new BufferedReader(new InputStreamReader(is) );
 		    
-		    JAXBContext context = JAXBContext.newInstance( Gols.class );
+		    /*JAXBContext context = JAXBContext.newInstance( Gols.class );
 		    Unmarshaller unMarshaller = context.createUnmarshaller();
-		    Gols param = (Gols) unMarshaller.unmarshal( br );
-		    
-		    
+		    Gols param = (Gols) unMarshaller.unmarshal( br );*/
+		    //String xmlString = response.body().string();
+		    JAXBContext jaxbContext = JAXBContext.newInstance(Gols.class);
+		    Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+		    StringReader reader = new StringReader( xmlString );
+		    Gols gols = (Gols) unmarshaller.unmarshal(reader);
+		    gols.getGols().get(0).mostrar();
 		    
 		    // Do something with the response.
 		} catch (IOException e) {
